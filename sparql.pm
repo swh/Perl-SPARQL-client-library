a0c6a0df848d8679330ed5df762c9011bd0a14d7
Mischa Tuffield <mischa@mmt.me.uk>
2011-07-29 17:47:21 +0100

    Removed tabs and substituted them with 4 spaces

--- sparql.pm
+++ sparql.pm
@@ -7,167 +7,169 @@ use XML::Simple;
 use strict;
 
 sub new {
-	my $class = shift;
-	my $ua = LWP::UserAgent->new(keep_alive => 1);
-	$ua->timeout(60);
-	$ua->conn_cache(LWP::ConnCache->new());
-	$ua->conn_cache->total_capacity(10);
-	$ua->agent("sparql.pm/1.1");
-	my $self = {
-		"_ua" => $ua
-	};
-	bless $self, $class;
+    my $class = shift;
+    my $ua = LWP::UserAgent->new(keep_alive => 1);
+    $ua->timeout(60);
+    $ua->conn_cache(LWP::ConnCache->new());
+    $ua->conn_cache->total_capacity(10);
+    $ua->agent("sparql.pm/1.1");
+    my $self = {
+        "_ua" => $ua
+    };
+    bless $self, $class;
 
-	return $self
+    return $self
 }
 
 sub query {
-	my ($self, $uri, $tquery) = @_;
+    my ($self, $uri, $tquery) = @_;
 
-	if (!$uri) {
-		warn "sparql_query() called without endpoint";
-		return ();
-	}
+    if (!$uri) {
+        warn "sparql_query() called without endpoint";
+        return ();
+    }
 
-	my $query = uri_escape($tquery);
-	my $ruri;
-	if ($uri =~ /\?/) {
-		$ruri = $uri.'&query='.$query;
-	} else {
-		$ruri = $uri.'?query='.$query;
-	}
+    my $query = uri_escape($tquery);
+    my $ruri;
+    if ($uri =~ /\?/) {
+        $ruri = $uri.'&query='.$query;
+    } else {
+        $ruri = $uri.'?query='.$query;
+    }
 
-	my $ret;
-	my $response = $self->{'_ua'}->get($ruri, 'Accept' => 'text/tab-separated-values, application/sparql-results+xml, application/x-turtle, text/rdf+n3');
-	if ($response->is_success) {
-		$ret = $response->decoded_content;
-	} elsif ($response->header("Client-Aborted")) {
-		warn "Response timeout";
+    my $ret;
+    my $response = $self->{'_ua'}->get($ruri, 'Accept' => 'text/tab-separated-values, application/sparql-results+xml, application/x-turtle, text/rdf+n3');
+    if ($response->is_success) {
+        $ret = $response->decoded_content;
+    } elsif ($response->header("Client-Aborted")) {
+        warn "Response timeout";
 
-		return ();
-	} else {
-		warn $response->status_line.": ".$tquery;
+        return ();
+    } else {
+        warn $response->status_line.": ".$tquery;
 
-		return ();
-	}
+        return ();
+    }
 
-	my $tsv = ($response->header('Content-Type') =~ m/^text\/tab-separated-values/i);
-	my $sr = ($response->header('Content-Type') =~ m/^application\/sparql-results\+xml/i);
-	my @retrows = ();
-	if ($tsv) {
-		my @rows = split("\n", $ret);
-		my $header = shift @rows;
-		if (!$header) {
-			warn("no header from query");
-			return ();
-		}
-		$header =~ s/^\s+//;
-		my @cols = grep(s/^[\?\$]//, split("\t", $header));
-		while (my $row = shift @rows) {
-			if ($row =~ /^#/) {
-				warn("SPARQL warning: $row\n");
-				next;
-			}
-			next if ($row =~ /^\s*$/);
-			my @data = split("\t", $row);
-			my %row = ();
-			for my $h (@cols) {
-				my $cell = shift @data;
-				$row{$h} = $cell;
-			}
-			push(@retrows, \%row);
-		}
+    my $tsv = ($response->header('Content-Type') =~ m/^text\/tab-separated-values/i);
+    my $sr = ($response->header('Content-Type') =~ m/^application\/sparql-results\+xml/i);
+    my @retrows = ();
+    if ($tsv) {
+        my @rows = split("\n", $ret);
+        my $header = shift @rows;
+        if (!$header) {
+            warn("no header from query");
+            return ();
+        }
+        $header =~ s/^\s+//;
+        my @cols = grep(s/^[\?\$]//, split("\t", $header));
+        while (my $row = shift @rows) {
+            if ($row =~ /^#/) {
+                warn("SPARQL warning: $row\n");
+                next;
+            }
+            next if ($row =~ /^\s*$/);
+            my @data = split("\t", $row);
+            my %row = ();
+            for my $h (@cols) {
+                my $cell = shift @data;
+                $row{$h} = $cell;
+            }
+            push(@retrows, \%row);
+        }
 
-		return \@retrows;
-	} elsif ($sr) {
-		my $tree = XMLin($ret, ForceArray => 1);
-		#print Dumper($tree)."\n";
-		my @retrows;
+        return \@retrows;
+    } elsif ($sr) {
+        my $tree = XMLin($ret, ForceArray => 1);
+        #print Dumper($tree)."\n";
+        my @retrows;
 
-		my $rows = $tree->{'results'}->[0]->{'result'};
-		for my $row (@{ $rows }) {
-			my %row;
-			for my $binding (keys %{$row->{'binding'}}) {
-				#print($binding."\n");
-				my $val = $row->{'binding'}->{$binding};
-				if ($val->{'uri'}) {
-					$row{$binding} = '<'.$val->{'uri'}->[0].'>';
-				} elsif ($val->{'literal'}) {
-					$row{$binding} = '"'.$val->{'literal'}->[0].'"';
-				} elsif ($val->{'bnode'}) {
-					$row{$binding} = '_:'.$val->{'literal'}->[0];
-				} else {
-					$row{$binding} = '???';
-				}
-			}
-			push(@retrows, \%row);
-		}
-		
-		return \@retrows;
-	}
+        my $rows = $tree->{'results'}->[0]->{'result'};
+        for my $row (@{ $rows }) {
+            my %row;
+            for my $binding (keys %{$row->{'binding'}}) {
+                #print($binding."\n");
+                my $val = $row->{'binding'}->{$binding};
+                if ($val->{'uri'}) {
+                    $row{$binding} = '<'.$val->{'uri'}->[0].'>';
+                } elsif ($val->{'literal'}) {
+                    $row{$binding} = '"'.$val->{'literal'}->[0].'"';
+                } elsif ($val->{'bnode'}) {
+                    $row{$binding} = '_:'.$val->{'literal'}->[0];
+                } else {
+                    $row{$binding} = '???';
+                }
+            }
+            push(@retrows, \%row);
+        }
+        
+        return \@retrows;
+    }
 
-	return $ret;
+    return $ret;
 }
 
 sub update {
-	my ($self, $uri, $tupdate) = @_;
+    my ($self, $uri, $tupdate) = @_;
 
-	if (!$uri) {
-		warn "SPARQL::update() called without endpoint";
-		return ();
-	}
+    if (!$uri) {
+        warn "SPARQL::update() called without endpoint";
+        return ();
+    }
 
-	my $ret;
-	my %form = ( 'update' => $tupdate );
-	my $response = $self->{'_ua'}->post($uri, \%form);
-	if ($response->is_success) {
-		$ret = $response->decoded_content;
-	} elsif ($response->header("Client-Aborted")) {
-		warn "Response timeout";
+    my $ret;
+    my %form = ( 'update' => $tupdate );
+    my $response = $self->{'_ua'}->post($uri, \%form);
+    if ($response->is_success) {
+        $ret = $response->decoded_content;
+    } elsif ($response->header("Client-Aborted")) {
+        warn "Response timeout";
 
-		return undef;
-	} else {
-		warn $response->status_line.": ".$tupdate."\n".$response->decoded_content;
+        return undef;
+    } else {
+        warn $response->status_line.": ".$tupdate."\n".$response->decoded_content;
 
-		return undef;
-	}
+        return undef;
+    }
 
-	return $ret;
+    return $ret;
 }
 
 sub print {
-	my ($self, $res) = @_;
+    my ($self, $res) = @_;
 
-	for my $row (@{$res}) {
-		my $first = 1;
-		for my $col (keys %{$row}) {
-			if ($first) {
-				$first = 0;
-			} else {
-				print(", ");
-			}
-			print('?'.$col.' = '.$row->{$col});
-		}
-		print("\n");
-	}
+    for my $row (@{$res}) {
+        my $first = 1;
+        for my $col (keys %{$row}) {
+            if ($first) {
+                $first = 0;
+            } else {
+                print(", ");
+            }
+            print('?'.$col.' = '.$row->{$col});
+        }
+        print("\n");
+    }
 }
 
 sub chomp {
-	my($self, $s) = @_;
+    my($self, $s) = @_;
 
-	if (!$s) {
-		return "";
-	}
+    if (!$s) {
+        return "";
+    }
 
-	if ($s =~ /^<(.*)>$/) {
-		return $1;
-	} elsif ($s =~ /"(.*)"/) {
-		return $1;
-	} elsif ($s =~ /"(.*)"^^<.*>$/) {
-		return $1;
-	}
+    if ($s =~ /^<(.*)>$/) {
+        return $1;
+    } elsif ($s =~ /"(.*)"/) {
+        return $1;
+    } elsif ($s =~ /"(.*)"^^<.*>$/) {
+        return $1;
+    }
 
-	return $s;
+    return $s;
 }
 
 1;
+
+# vi:set expandtab sts=4 sw=4:
--- test.pl
+++ test.pl
@@ -9,8 +9,10 @@ my $res = $sparql->query('http://dbpedia.org/sparql',
                          'SELECT * WHERE { ?s ?p ?o } LIMIT 10');
 
 for my $row (@{$res}) {
-	for my $col (keys %{$row}) {
-		print('?'.$col.' = '.$row->{$col}."\n");
-	}
-	print("\n");
+    for my $col (keys %{$row}) {
+        print('?'.$col.' = '.$row->{$col}."\n");
+    }
+    print("\n");
 }
+
+# vi:set expandtab sts=4 sw=4:
